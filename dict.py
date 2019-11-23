21409c4fcfb30c0094c140176c4578ee4d9a4bce
Shruthi-Yamsani <58043909+Shruthi-Yamsani@users.noreply.github.com>
2019-11-23 13:25:02 +0530

    Update dict.py
    Bug fixes in Play.

--- dict.py
+++ dict.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import random
 
 # API details
 apihost = 'https://fourtytwowords.herokuapp.com'
@@ -7,25 +8,79 @@ apikey = "b972c7ca44dda72a5b482052b1f5e13470e01477f3fb97c85d5313b3c1126270734811
 action_endpoints = {"defn": "definitions", "ex": "examples", "syn": "relatedWords", "ant": "relatedWords", "play": "",
                     "wod": "randomWord"}
 
-# Read User input
+def pickRandomWord():
+	url = apihost + "/words/" + action_endpoints["wod"] + "?api_key=" + apikey
+	response = requests.get(url)
+	return response.json()["word"]
+	
+def play(defn, syn, ant, ex, word, number_trial):
+    index = number_trial - 1
+    print("Guess the word based on below details")
+    
+    if defn:
+        print("The definition is:")
+        if index < len(defn):
+            print (defn[index])
+        else:
+            print (defn[0])
+    
+    if syn:
+        print("The synonym is:")
+        if index < len(syn):
+            print (syn[index])
+        else:
+            print (syn[0])
+    
+    if ant:
+        print("The antonym is:")
+        if index < len(ant):
+            print (ant[index])
+        else:
+            print (ant[0])
+            
+    #Read User input as answer
+    answer = input("Enter your Answer: ")
+    #Validate user answer
+    if answer == word:
+        print("Success! You won!")
+    else:
+        print("Wrong Answer")
+        print("Press 1 to Try Again")
+        print("Press 2 if you need a hint")
+        print("Press 3 to Quit")
+        choice = input()
+        if choice == "1":
+            actions = ["defn", "syn", "ant", "ex"]
+            word = pickRandomWord()
+            play(defn, syn, ant, ex, word, number_trial+1)
+        elif choice == "2":
+            print("The word in jumbled format: ",''.join(random.sample(word,len(word))))
+            play(defn, syn, ant, ex, word, number_trial+1)
+        else:
+            print("You are out now! Thanks for trying us")
+
+#Read User input
 actions = None
 word = None
 isPlay = False
 number_of_inputs = len(sys.argv) - 1
 if number_of_inputs >= 1:
-    argv1 = sys.argv[1]
-    if argv1 == "play":
-        isPlay = True
-        actions = ["defn", "syn", "ant", "ex"]
-    elif argv1 in action_endpoints:
-        actions = [argv1]
-        word = sys.argv[2]
-    else:
-        actions = ["defn", "syn", "ant", "ex"]
-        word = argv1
+	argv1 = sys.argv[1]
+	if argv1 == "play":
+		isPlay = True
+		actions = ["defn", "syn", "ant", "ex"]
+		word = pickRandomWord()
+	elif argv1 in action_endpoints:
+		actions = [argv1]
+		word = sys.argv[2]
+	else:
+		actions = ["defn", "syn", "ant", "ex"]
+		word = argv1
 else:
-    actions = ["defn", "syn", "ant", "ex"]
-    word = pickRandomWord()
+	actions = ["defn", "syn", "ant", "ex"]
+	word = pickRandomWord()
+	
+	
 
 # Implement respective API for given input
 defn = []
@@ -59,69 +114,23 @@ if actions is not None:
                 print("Error connecting ", action_endpoints[action], " End point")
 
 if not isPlay:
-    if not defn:
-        print("\n Definition of ", word.capitalize(), "\n")
+    if len(defn)> 0:
+        print("\n Definition of ",word.capitalize(), "\n")
         for d in defn:
-            print(d)
-    if not syn:
-        print("\n Synonyms for word ", word.capitalize(), "\n")
+            print (d)
+    if len(syn)>0:
+        print("\n Synonyms for word ",word.capitalize(), "\n")
         for s in syn:
-            print(s)
-    if not ant:
-        print("\n Antonyms for word ", word.capitalize(), "\n")
+            print (s)
+    if len(ant)>0:
+        print("\n Antonyms for word ",word.capitalize(), "\n")
         for a in ant:
-            print(a)
-    if not ex:
-        print("\n Examples for word ", word.capitalize(), "\n")
+            print (a)
+    if len(ex)>0:
+        print("\n Examples for word ",word.capitalize(), "\n")
         for e in ex:
-            print(e)
-
+            print (e)
+            
 else:
     number_trial = 1
-    play(defn, syn, ant, ex, number_trial)
-
-
-def play(defn, syn, ant, ex, number_trial):
-    index = number_trial - 1
-    print("Guess the word based on below details")
-    print("The definition is:")
-    if defn:
-        if index < len(defn):
-            print(defn[index])
-        else:
-            print(defn[0])
-    if syn:
-        if index < len(syn):
-            print(syn[index])
-        else:
-            print(syn[0])
-    if ant:
-        if index < len(ant):
-            print(ant[index])
-        else:
-            print(ant[0])
-
-    # Read User input as answer
-    answer = input("Enter your Answer")
-    # Validate user answer
-    if answer == word:
-        print("Success")
-    else:
-        print("Wrong Answer")
-        print("Press 1 to Try Again")
-        print("Press 2 if you need a hint")
-        print("Press 3 to Quit")
-        choice = input()
-        if choice == 1:
-            play(defn, syn, ant, ex, number_trial + 1)
-        elif choice == 2:
-            actions = ["defn", "syn", "ant", "ex"]
-            word = pickRandomWord()
-        else:
-            print("You are out now! Thanks for trying us")
-
-
-def pickRandomWord():
-    url = apihost + "/words/" + action_endpoints["wod"] + "?api_key=" + apikey
-    response = requests.get(url)
-    return response.json()["word"]
+    play(defn, syn, ant, ex, word, number_trial)
